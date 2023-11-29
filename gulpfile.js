579d08b6a9c3e37fba3c6c1ec49f7c991581a5dc
list91 <s_anufriev@list.ru>
2023-11-29 09:00:57 +0700

    init

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const watch = require('gulp-watch');
+
+gulp.task('ss', function() {
+  return gulp.src('./sass/main.sass')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task("watch", function(){
+    gulp.watch("./sass/**/*.sass", gulp.series("ss"))
+});
